da43ae7d190bcabd0d64930cd1dd4d5caa448d81
Henry K <hdkno577@learner.dvusd.org>
2022-12-01 09:19:24 -0700

    Roll-a-Ball3
    Ball moves and is now Walter White

--- Roll-a-Ball/Assets/Script/BallController.cs
+++ Roll-a-Ball/Assets/Script/BallController.cs
@@ -5,7 +5,10 @@ using UnityEngine.InputSystem;
 
 public class BallController : MonoBehaviour
 {
+    public float speed = 0;
     private Rigidbody rb;
+    private float moveX;
+    private float moveY;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class BallController : MonoBehaviour
     }
     void OnMove(InputValue movementValue)
     {
-        Vector2 movemenntVector = movementValue.get<Vector2>();
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        moveX = movementVector.x;
+        moveY = movementVector.y;
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveX, 0.0f, moveY);
+        rb.AddForce(movement * speed);
     }
 }
